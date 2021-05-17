168c4ae19a43905f49c0c8020d10f9fe0fd0b7af
Sławomir Świętoniowski <slawomir.swietoniowski@gmail.com>
2021-05-17 16:22:10 +0200

    Update parsers.py

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -28,5 +28,5 @@ class Parser:
 class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css", ".html"]
 
-    def parse(self, path:'Path', source:'Path', dest:'Path'):
+    def parse(self, path: 'Path', source: 'Path', dest: 'Path'):
         self.copy(path, source, dest)
